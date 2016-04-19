f890f279ab444d409b7ef61d617c5ac69f83cf33
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-04-19 13:12:36 +0300

    Global catalog parsing done

--- src/Main.java
+++ src/Main.java
@@ -10,49 +10,61 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
-        String url = "http://glavbritva.ru/product_list/page_1?product_items_per_page=48";
-
+        String host = "http://glavbritva.ru";
+        String url = host + "/product_list?product_items_per_page=48";
         Document doc = Jsoup.connect(url).get();
+        Elements firstStage = doc.select("div[class=b-pager]");
+        int pagesCount = Integer.parseInt(firstStage.first().select("a[class= b-pager__link ]").last().text());
+        System.out.println("Total pages: " + pagesCount);
 
+        String pagesLinks[] = new String[pagesCount];
+
+        for (int i = 0; i < pagesCount; i++) {
+            pagesLinks[i] = host + "/product_list/page_" + (i+1) + "?product_items_per_page=48";
+            System.out.println(pagesLinks[i]);
+        }
+
+        int n = 0;
+
+        for (String pagesLink : pagesLinks) {
+            n += parseItems(pagesLink);
+        }
+
+        System.out.println("Total items: " + n);
+    }
+
+    static int parseItems(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
         Elements firstStage = doc.select("div[class=b-layout__clear]");
-
         Elements items = firstStage.first().select("div[class~=(b-product-line b-product-line_type_gallery js-partner-criteo|b-product-line b-product-line_type_gallery b-product-line_pos_last js-partner-criteo)]");
 
         int n = 0;
 
         for (Element item:items) {
             n++;
-
             String id = item.select("span").first().attr("title");
             String link = item.select("a").first().attr("href");
             Element imageElement = item.select("img").first();
             String image = imageElement.attr("src");
             String description = imageElement.attr("alt");
-
             float price = priceToFloat(item.select("div[class=b-product-line__price]").first().text());
-
-
             String available = item.select("span[class=b-product-line__state]").first().text();
 
             System.out.println( "ID: "+ id + "\n" +
-                                "Link: " + link + "\n" +
-                                "Image: " + image + "\n" +
-                                "Description: " + description + "\n" +
-                                "Price: " + price + "\n" +
-                                "Available: " + available + "\n" + "\n"
-                                );
+                    "Link: " + link + "\n" +
+                    "Image: " + image + "\n" +
+                    "Description: " + description + "\n" +
+                    "Price: " + price + "\n" +
+                    "Available: " + available + "\n"
+            );
         }
-
-        System.out.println("Total items:" + n);
-
+        return n;
     }
 
-    static float priceToFloat(String price){
 
+    static float priceToFloat(String price){
         Pattern pattern = Pattern.compile("(\\d*)(\\.*)(\\d*)");
         Matcher matcher = pattern.matcher(price.replace(',','.'));
-
         float floatPrice = 0;
 
         if (matcher.find()){
