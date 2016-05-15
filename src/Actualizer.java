260f5aa4e70aa6f1e1b8d789ccd5285ee911cb0d
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-05-16 01:54:14 +0300

    actualize() method is now accept only HashMap with parsed products. actualize() method is also checks if product is present in supplier shop. Added errors summary report.

--- src/Actualizer.java
+++ src/Actualizer.java
@@ -1,59 +1,92 @@
 import database.DBWorker;
 import beans.Product;
+import vk.VKWorker;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Actualizer {
 
-    public static void actualize(ArrayList<Product> parsedProducts, TreeMap<String, Product> dbProducts){
+    public static void actualize(HashMap<String, Product> parsedProducts){
 
+        TreeMap<String, Product> dbProducts = DBWorker.getAllProducts();
         ArrayList<Product> updatedProducts = new ArrayList<>();
         ArrayList<Product> addedProducts = new ArrayList<>();
+        ArrayList<Product> errorProducts = new ArrayList<>();
 
-        for (Product parsedProduct : parsedProducts) {
+        // Проверяем, есть ли вообще у поставщика наши товары
+        for (Map.Entry<String, Product> dbProductEntry : dbProducts.entrySet()) {
 
-            // Если спаршеный продукт уже есть в базе
-            if (dbProducts.containsKey(parsedProduct.getId())) {
+            Product dbProduct = dbProductEntry.getValue();
 
-                Product dbProduct = dbProducts.get(parsedProduct.getId()); // Получаем соответствующий товар из БД
-                String ppId = parsedProduct.getId();
-                String dbpId = dbProduct.getId();
-
-                BigDecimal ppPrice = parsedProduct.getPrice();
-                BigDecimal dbpPrice = dbProduct.getPrice();
-                int ppDeleted = parsedProduct.getDeleted();
-                int dbpDeleted = dbProduct.getDeleted();
-
-                if (!ppPrice.equals(dbpPrice) || ppDeleted != dbpDeleted) {
-                    System.out.println("Updating product: " + parsedProduct.getId());
-                    DBWorker.updateProduct(parsedProduct);
-                    updatedProducts.add(parsedProduct);
-                    // TODO: vk update product
+            // Если у поставщика больше нет нашего товара, который доступен в группе ВК
+            if (!parsedProducts.containsKey(dbProduct.getId()) & dbProduct.getDeleted() == 0) {
+                dbProduct.setDeleted(2);
+                //Убираем товар в группе ВК из наличия. Если успешно...
+                if(VKWorker.updateProduct(dbProduct)){
+                    //Обновляем статус товара в БД
+                    DBWorker.updateProduct(dbProduct);
                 }
-            }
-            // Если продукт новый и его нет в базе
-            else {
-                System.out.println("Adding new product: " + parsedProduct.getId());
-                DBWorker.addProduct(parsedProduct);
-                addedProducts.add(parsedProduct);
-                // TODO: vk add product
             }
 
         }
 
-//        for (Product product : updatedProducts) {
-//            System.out.println(product + "\n");
-//        }
-        System.out.println("Products updated: " + updatedProducts.size());
+        // Проверяем остальные товары БД на актуальность (цена и наличие)
+        for (Map.Entry<String, Product> parsedProductEntry : parsedProducts.entrySet()) {
 
-//        for (Product product : addedProducts) {
-//            System.out.println(product + "\n");
-//        }
+            Product parsedProduct = parsedProductEntry.getValue();
+
+            // Если продукт уже есть в базе
+            if (dbProducts.containsKey(parsedProduct.getId())) {
+
+                Product dbProduct = dbProducts.get(parsedProduct.getId());
+
+                BigDecimal parsedProductPrice = parsedProduct.getPrice();
+                BigDecimal dbProductPrice = dbProduct.getPrice();
+                int parsedProductDeleted = parsedProduct.getDeleted();
+                int dbProductDeleted = dbProduct.getDeleted();
+
+                // Если цена или наличие в базе не актуальны
+                if (!parsedProductPrice.equals(dbProductPrice) || parsedProductDeleted != dbProductDeleted) {
+                    dbProduct.setPrice(parsedProductPrice);
+                    dbProduct.setDeleted(parsedProductDeleted);
+
+                    // Обновляем товар в группе ВК. Если успешно...
+                    if(VKWorker.updateProduct(dbProduct)){
+                        // Обновляем товар в БД
+                        DBWorker.updateProduct(dbProduct);
+                        updatedProducts.add(dbProduct);
+                    } else {
+                        errorProducts.add(dbProduct);
+                    }
+                }
+            }
+            // Если продукт новый и его нет в базе
+            else {
+                // Добавляем продукт в группу ВК. Если успешно...
+                Product addedProduct = VKWorker.addProduct(parsedProduct);
+                if (addedProduct != null) {
+                    // Добавляем продукт в БД.
+                    DBWorker.addProduct(addedProduct);
+                    addedProducts.add(addedProduct);
+                } else {
+                    errorProducts.add(parsedProduct);
+                }
+            }
+
+        }
+
+        System.out.println("Products updated: " + updatedProducts.size());
         System.out.println("New products added: " + addedProducts.size());
 
-    }
+        if (errorProducts.size() > 0){
+            System.out.println("There were problems with following products: " + errorProducts.size());
+            errorProducts.forEach(product -> System.out.println("\t" + product.getName()));
+        }
 
+    }
 
 }
