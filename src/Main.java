e53fdae770773a6cc799269c6ff8aa17142f2b7d
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-05-16 22:31:23 +0300

    Working on...

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,56 @@
 import beans.Product;
 import database.DBConnectionManager;
 import database.DBWorker;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import parser.ParserGlavbritva;
 import vk.VKWorker;
 
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
+//        HashMap<String, Product> parsedProducts = ParserGlavbritva.parseCategory();
+//        Actualizer.actualize(parsedProducts);
+//
+//        ArrayList<Product> unaddedProducts = DBWorker.getUnaddedProducts();
+//
+//        if (unaddedProducts.size() > 0) {
+//            System.out.println("Trying to add not added before products into VK group:");
+//            for (Product product : unaddedProducts) {
+//                Product addedProduct = VKWorker.addProduct(product);
+//                if (addedProduct != null) {
+//                    DBWorker.updateProduct(addedProduct);
+//                }
+//            }
+//        }
+//
+//        DBConnectionManager.closeConnection();
+
         HashMap<String, Product> parsedProducts = ParserGlavbritva.parse();
-        Actualizer.actualize(parsedProducts);
+        System.out.println("\n---------------------------\nTotal items parsed: " + parsedProducts.size());
 
-        ArrayList<Product> unaddedProducts = DBWorker.getUnaddedProducts();
 
-        if (unaddedProducts.size() > 0) {
-            System.out.println("Trying to add not added before products into VK group:");
-            for (Product product : unaddedProducts) {
-                Product addedProduct = VKWorker.addProduct(product);
-                if (addedProduct != null) {
-                    DBWorker.updateProduct(addedProduct);
-                }
-            }
-        }
 
-        DBConnectionManager.closeConnection();
+
+//        HashMap<String, Product> parsedProducts = ParserGlavbritva.parseCategory();
+//        parsedProducts.forEach((s, product) -> System.out.println(product.getId()));
+//        System.out.println("Total parsed products: " + parsedProducts.size());
+//
+//        for (String excludedProductId : ParserGlavbritva.excludedProducts) {
+//            System.out.println(excludedProductId);
+//            if (parsedProducts.containsKey(excludedProductId)) {
+//                System.out.println("Excluding product: " + parsedProducts.get(excludedProductId).getName());
+//                parsedProducts.remove(excludedProductId);
+//            }
+//        }
+//        System.out.println("Total products after excluding: " + parsedProducts.size());
+
+
 
 
 //        Product testProduct = new Product();
