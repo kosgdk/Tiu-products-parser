740d35af61d236e26eb5bc655558316b3521b493
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-05-25 22:12:55 +0300

    Changed groupID added delay changed price scale format and rounding mode added deleteProduct() and reorderProduct() methods

--- src/vk/VKWorker.java
+++ src/vk/VKWorker.java
@@ -9,14 +9,18 @@ import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
+import parser.ParserGlavbritva;
+
 import java.io.File;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.nio.charset.Charset;
+
 
 public class VKWorker {
 
     private static final String apiURL = "https://api.vk.com/method/";
-    private static final String groupId = "120338153";
+    private static final String groupId = "98658033";
     private static final BigDecimal priceMultiplyer = new BigDecimal(1.15);
 
     private static String token = Authorization.getToken();
@@ -24,8 +28,8 @@ public class VKWorker {
 
     public static Product addProduct(Product product){
 
-        System.out.print("Adding product to VK group... ");
-
+        ParserGlavbritva.sleep(400);
+        System.out.print("\nAdding product \"" + product.getName() + "\" to VK group... ");
 
         // Проверяем фото на валидность и соответствие размеру (не мене 400х400)
         String imageLink = product.getImageLink();
@@ -39,7 +43,7 @@ public class VKWorker {
                 .addTextBody("name", cropName(product.getName()), ContentType.create("text/plain", Charset.forName("UTF-8")))
                 .addTextBody("description", product.getName(), ContentType.create("text/plain", Charset.forName("UTF-8")))
                 .addTextBody("category_id", String.valueOf(categoryId))
-                .addTextBody("price", String.valueOf(product.getPrice().multiply(priceMultiplyer)))
+                .addTextBody("price", String.valueOf(product.getPrice().multiply(priceMultiplyer).setScale(0, RoundingMode.HALF_UP)))
                 .addTextBody("deleted", String.valueOf(product.getDeleted()))
                 .addTextBody("main_photo_id", String.valueOf(product.getVkPhotoId()))
                 .build();
@@ -54,15 +58,11 @@ public class VKWorker {
             JsonParser JSONparser = new JsonParser();
             int vkID = JSONparser.parse(response).getAsJsonObject().getAsJsonObject("response").get("market_item_id").getAsInt();
             product.setVkId(vkID);
-
-            // Обновляем продукт в БД - добавляем VkID и VkPhotoId
-//            DBWorker.updateProduct(product);
-
             return product;
 
         } else {
             System.out.println("fail");
-            System.out.println("VK response: " + response + "\n");
+            System.out.println("VK response: " + response);
             return null;
         }
 
@@ -70,7 +70,8 @@ public class VKWorker {
 
     public static boolean updateProduct(Product product){
 
-        System.out.println("Updating product \"" + product.getName() + "\" in VK group:");
+        ParserGlavbritva.sleep(400);
+        System.out.println("---\nUpdating product \"" + product.getName() + "\" in VK group:");
 
         product = updateDbVkPhotoId(product);
 
@@ -83,7 +84,7 @@ public class VKWorker {
                 .addTextBody("name", cropName(product.getName()), ContentType.create("text/plain", Charset.forName("UTF-8")))
                 .addTextBody("description", product.getName(), ContentType.create("text/plain", Charset.forName("UTF-8")))
                 .addTextBody("category_id", String.valueOf(categoryId))
-                .addTextBody("price", String.valueOf(product.getPrice().multiply(priceMultiplyer)))
+                .addTextBody("price", String.valueOf(product.getPrice().multiply(priceMultiplyer).setScale(0, RoundingMode.HALF_UP)))
                 .addTextBody("deleted", String.valueOf(product.getDeleted()))
                 .addTextBody("main_photo_id", String.valueOf(product.getVkPhotoId()))
                 .build();
@@ -98,6 +99,99 @@ public class VKWorker {
         } else {
             System.out.println("fail.");
             System.out.println("VK server response: " + response);
+            return false;
+        }
+
+    }
+
+    public static boolean deleteProduct(Product product){
+
+        ParserGlavbritva.sleep(100);
+
+        System.out.print("Deleting product \"" + product.getName() + "\" from VK group... ");
+
+        // Создаём запрос на удаление товара
+        HttpPost httppost = new HttpPost("https://api.vk.com/method/market.delete");
+        HttpEntity httpEntity = MultipartEntityBuilder.create()
+                .addTextBody("access_token", token)
+                .addTextBody("owner_id", "-" + groupId)
+                .addTextBody("item_id", String.valueOf(product.getVkId()))
+                .build();
+        httppost.setEntity(httpEntity);
+
+        // Отправляем запрос
+        String response = POSTSender.send(httppost);
+
+        // Проверяем, добавился ли товар
+        if (response != null && response.contains("{\"response\":1}")) {
+            System.out.println("success!");
+            return true;
+        } else {
+            System.out.println("fail");
+            System.out.println("VK response: " + response + "\n");
+            return false;
+        }
+
+    }
+
+    public static boolean reorderProduct(String albumId, String vkId, String afterId){
+
+        ParserGlavbritva.sleep(200);
+
+        System.out.print("Reordering product \"" + vkId + "\" position ... ");
+
+        // Создаём запрос на перемещение продукта в подборке
+        HttpPost httppost = new HttpPost("https://api.vk.com/method/market.reorderItems");
+        HttpEntity httpEntity = MultipartEntityBuilder.create()
+                .addTextBody("access_token", token)
+                .addTextBody("owner_id", "-" + groupId)
+                .addTextBody("album_id", albumId)
+                .addTextBody("item_id", vkId)
+                .addTextBody("after", afterId)
+                .build();
+        httppost.setEntity(httpEntity);
+
+        // Отправляем запрос
+        String response = POSTSender.send(httppost);
+
+        // Проверяем, добавился ли товар
+        if (response != null && response.contains("{\"response\":1}")) {
+            System.out.println("success!");
+            return true;
+        } else {
+            System.out.println("fail");
+            System.out.println("VK response: " + response + "\n");
+            return false;
+        }
+
+    }
+
+    public static boolean setCategory(Product product, int categoryId){
+
+        ParserGlavbritva.sleep(200);
+
+        System.out.print("Adding product \"" + product.getName() + "\" to VK album... ");
+
+        // Создаём запрос на удаление товара
+        HttpPost httppost = new HttpPost("https://api.vk.com/method/market.addToAlbum");
+        HttpEntity httpEntity = MultipartEntityBuilder.create()
+                .addTextBody("access_token", token)
+                .addTextBody("owner_id", "-" + groupId)
+                .addTextBody("item_id", String.valueOf(product.getVkId()))
+                .addTextBody("album_ids", String.valueOf(categoryId))
+                .build();
+        httppost.setEntity(httpEntity);
+
+        // Отправляем запрос
+        String response = POSTSender.send(httppost);
+
+        // Проверяем на успешность операции
+        if (response != null && response.contains("{\"response\":1}")) {
+            System.out.println("success!");
+            return true;
+        } else {
+            System.out.println("fail");
+            System.out.println("VK response: " + response + "\n");
             return false;
         }
 
@@ -172,7 +266,7 @@ public class VKWorker {
         int length = name.length();
         int escapedLength = name.replace("\"","\\\"").length();
 
-        if (escapedLength > 98){
+        if (escapedLength > 95){
             return name.substring(0, 95-(escapedLength-length))+"...";
         } else {
             return name;
